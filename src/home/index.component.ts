bf348ada2fcac4d446dccaddce99a3696e4c59b4
Habibi Gusti Pangestu <habibi.gusti@qoala.id>
2024-03-10 22:22:17 +0700

    feat: homepage

---
+++ src/home/index.component.ts
@@ -0,0 +1,11 @@
+import AppHeader from "@/components/AppHeader.vue";
+
+export default defineComponent({
+    name: "Home",
+    components: {
+      AppHeader,
+    },
+    setup() {
+        return {}
+    },
+  })
